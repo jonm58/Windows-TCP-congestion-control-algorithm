87aa363eec3a3242f0bb0068ffe39dbc9be53679
結束バンドbocchi <73036321+jonm58@users.noreply.github.com>
2024-10-05 13:42:29 +0800

    Create use_BBR2.cmd

---
+++ use_BBR2.cmd
@@ -0,0 +1,5 @@
+netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2
+netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2
+netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2
+netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2
+netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2
